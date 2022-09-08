4612c95bd930c34354e8ea44709cdfe285f4540f
Pauline Thomas <pauline.farina90@gmail.com>
2022-09-08 16:47:33 +0200

    login and verify route done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -74,9 +74,54 @@ router.post('/signup', (req, res) => {
 });
 
 // LOGIN - POST
-router.post('/login', (req, res) => {});
+router.post('/login', (req, res) => {
+    const { password, email } = req.body;
+
+    //check if all fields are completed
+    if (email === '' || password === '') {
+        res.status(400).json({ message: 'All fields are mandatory' });
+        return;
+    }
+
+    //User in DB check
+    User.findOne({ email })
+        .then((foundUser) => {
+            if (!foundUser) {
+                res.status(400).json({
+                    message: 'No account found with this email address',
+                });
+                return;
+            }
+
+            // Correct Password Check
+            const passwordCorrect = bcrypt.compareSync(
+                password,
+                foundUser.password
+            );
+
+            //Create Sign in Token
+            if (passwordCorrect) {
+                const { _id, email, username } = foundUser;
+                const payload = { _id, email, username };
+
+                const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
+                    algorithm: 'HS256',
+                    expiresIn: '6h',
+                });
+                //Send token as response
+                res.status(200).json({ authToken: authToken });
+            } else {
+                res.status(401).json({ message: 'Unable to authenticate' });
+            }
+        })
+        .catch((err) =>
+            res.status(500).json({ message: 'Internal Server Error' })
+        );
+});
 
 // VERIFY - POST
-router.post('/verify', (req, res) => {});
+router.post('/verify', (req, res) => {
+    res.status(200).json(req.payload);
+});
 
 module.exports = router;
