ac8d66b018d28596fbf8086026fc28f9180c867b
Pauline Thomas <pauline.farina90@gmail.com>
2022-09-15 14:46:49 +0200

    cors fix

--- config/index.js
+++ config/index.js
@@ -21,12 +21,7 @@ module.exports = (app) => {
     app.set('trust proxy', 1);
 
     // controls a very specific header to pass headers from the frontend
-    app.use(
-        cors({
-            credentials: true,
-            origin: process.env.ORIGIN || 'http://localhost:3001',
-        })
-    );
+    app.use(cors());
 
     // In development environment the app logs
     app.use(logger('dev'));
